cd430a0b250ad2d18e30963e06f88343b1041baa
DenysTantiura <111456850+DenysTantsiura@users.noreply.github.com>
2022-11-20 19:38:11 +0200

    something fixing
    something fixing

---
+++ flash_freespace_placeholder.py
@@ -0,0 +1,86 @@
+# Be careful ! Analyze the code first!
+import os
+import shutil
+
+
+KB = 1024
+MB = 1024 * KB
+GB = 1024 * MB
+
+# If run in root of disk
+
+disk = os.fspath(os.path.abspath(os.getcwd()))
+
+os.makedirs(os.fspath('bin-free-bin'), exist_ok=True)
+
+ballast_file = os.fspath(os.path.join(disk, 'bin-free-bin'))
+
+print(disk)
+
+print(ballast_file)
+
+print(f'Free space on disk \"{disk}\", Kb: ', shutil.disk_usage('/').free / KB)
+
+free_size = shutil.disk_usage('/').free  # bytes
+
+block = '0'
+big_files_count = free_size // (2 * GB)
+small_file_size = free_size % (2 * GB)
+input(f'2 GB Files: {big_files_count}')
+print(f'And 1 file {small_file_size} byte')
+
+
+def drop_file(cx, file_number):
+
+    file = os.fspath(os.path.join(ballast_file, f'F{file_number}.data'))
+
+    try:
+
+        with open(file, 'wb') as fh:
+
+            fh.write(bytes(block*cx, 'utf-8'))
+
+    except Exception as err_:
+
+        print(f'Oops, exception =) : {repr(err_)}') 
+
+        input('Press Enter to exit...')
+
+        return False   
+
+    return True
+
+file_number = 0
+
+while drop_file(len(block)*2*GB, file_number):
+    
+    if not file_number % 2:
+        
+        print(file_number)
+        
+    file_number += 1
+
+print(new_free_size := shutil.disk_usage('/').free)  # bytes
+
+drop_file(new_free_size, file_number)
+
+input(f'Free space on disk \"{disk}\", Kb: ', shutil.disk_usage('/').free / KB)
+
+
+# possible_number_of_files = (shutil.disk_usage('/').free / KB) // 4
+
+# input(f'max files: {possible_number_of_files}')
+
+# file_number = 0
+
+# while drop_file(loop_cycle_step, file_number):
+    
+#     if not file_number % 10:
+        
+#         print(file_number)
+        
+#     file_number += 1
+
+# print(f'Free space on disk \"{disk}\", Kb: ', shutil.disk_usage('/').free / KB)
+
+# input('Press Enter to exit...')
